7ad025cb2a3b5e5761d656963f00e60a3c608964
OgasawaraShu <74286718+OgasawaraShu@users.noreply.github.com>
2023-04-26 16:45:13 +0900

    野菜を焼けるようになった

---
+++ Assets/YasaiFire.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YasaiFire : MonoBehaviour
+{
+    [SerializeField] Material[] materialArray = new Material[3];
+    enum fireLevel
+    {
+        NONE,
+        RARE,
+        WELLDONE,
+        KOGE
+    }
+    float firecount;
+
+    fireLevel level = fireLevel.NONE;
+
+    string Ami;//ñ‘Ç©Ç«Ç§Ç©ÇÃî‰är
+
+    // Start is called before the first frame update
+    void Start()//èâä˙âª
+    {
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (level < fireLevel.KOGE)
+        {
+            if (other.gameObject.tag == "Ami")
+            {
+                firecount += 1.0f;
+            }
+
+            if (firecount >= 240)
+            {
+                level++;
+                firecount = 0;
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()//çXêV
+    {
+
+        if (level == fireLevel.NONE)
+        {
+            GetComponent<MeshRenderer>().material = materialArray[0];
+        }
+
+        if (level == fireLevel.WELLDONE)
+        {
+            GetComponent<MeshRenderer>().material = materialArray[1];
+        }
+
+        if (level == fireLevel.KOGE)
+        {
+            GetComponent<MeshRenderer>().material = materialArray[2];
+        }
+
+    }
+}
