19f695d3b37b7730e21131c1233c2c02b6b19370
OgasawaraShu <74286718+OgasawaraShu@users.noreply.github.com>
2023-05-17 15:39:53 +0900

    ゲージ三つに増やした　空腹度自動で減る

--- Assets/Gage.cs
+++ Assets/Gage.cs
@@ -9,12 +9,20 @@ public class Gage : MonoBehaviour
 {
     //最大HPと現在のHP。
     int maxHp = 100;
-    int currentHp;
+
+    float currentHp;
+    float happyHp;
+    float healthHp;
     //public Eat eat;
     //Sliderを入れる
     public Slider slider;
+    public Slider happySlider;
+    public Slider healthSlider;
+
     public static Gage instance;
-    public int point;
+    public float point;
+    public float happyPoint;
+    public float healthPoint;
    // public string name;
 
     public void Awake()
@@ -27,15 +35,19 @@ public class Gage : MonoBehaviour
 
     void Start()
     {
-        //ポイントを0にする
-        point = 0;
+        //ポイントを50にする
+        point = 50;
+        happyPoint = 50;
+        healthPoint = 50;
         //Sliderを満タンにする。
-        slider.value = 0;
-        //現在のHPを0と同じに。
-        currentHp = 0;
+        slider.value = 50;
+        happySlider.value = 50;
+        healthSlider.value =50;
+        //現在のHPを50と同じに。
+        currentHp = 50;
+        happyHp = 50;
+        healthHp = 50;
         Debug.Log("Start currentHp : " + currentHp);
-
-       
     }
 
     private void OnMouseDown()
@@ -64,24 +76,43 @@ public class Gage : MonoBehaviour
         if(name.Contains("Karubi"))
         {
             point += 50;
-
+            happyPoint += 25;
+            healthPoint -= 10;
         }
         else if(name.Contains("kyabetu"))
         {
             point += 25;
+            happyPoint -= 10;
+            healthPoint += 25;
         }
         else
         {
             point += 10;
+            happyPoint += 10;
+            healthPoint -= 5;
         }
 
         Eat.instance.GetFlag = false;
         
     }
+
+
     void Update()
     {
-        
+
+       
+
         currentHp = point;
+
+        point -= 0.01f;
+        currentHp -= 0.01f;
+
+        happyHp = happyPoint;
+        healthHp = healthPoint;
+
+
         slider.value = (float)currentHp / (float)maxHp; ;
+        happySlider.value = (float)happyHp / (float)maxHp; ;
+        healthSlider.value = (float)healthHp / (float)maxHp; ;
     }
 }
