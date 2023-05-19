6d0cc4de4e46381df9bf8fcfc78309f80c6658ba
OgasawaraShu <74286718+OgasawaraShu@users.noreply.github.com>
2023-05-19 16:57:54 +0900

    五秒ごとに肉ができる

--- Assets/NikuPrefab.cs
+++ Assets/NikuPrefab.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class NikuPrefab : MonoBehaviour
 {
+    public float timeOut;
+    private float timeElapsed;
+
+
     // Use this for initialization
     void Start()
     {
+        timeOut = 5.0f;
+
         // CubeプレハブをGameObject型で取得
         GameObject obj = (GameObject)Resources.Load("niku2");
         // Cubeプレハブを元に、インスタンスを生成、
@@ -16,8 +22,20 @@ public class NikuPrefab : MonoBehaviour
     //更新
     void Update()
     {
-        //皿の上に置かれてない場所が存在するのならそこにランダムで生成する
-        //
+        timeElapsed += Time.deltaTime;
+
+        if (timeElapsed >= timeOut)
+        {
+            // Do anything
+            // CubeプレハブをGameObject型で取得
+            GameObject obj = (GameObject)Resources.Load("niku2");
+            // Cubeプレハブを元に、インスタンスを生成、
+            Instantiate(obj, new Vector3(2.2f, 2.3f, -7.0f), Quaternion.identity);
+
+
+            timeElapsed = 0.0f;
+        }
+ 
 
     }
 
