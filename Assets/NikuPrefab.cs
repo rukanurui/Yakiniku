0fa7b1e6431db55082d3cb90027069a73656e32c
OgasawaraShu <74286718+OgasawaraShu@users.noreply.github.com>
2023-06-07 17:10:19 +0900

    皿の上にないときランダムに生成

--- Assets/NikuPrefab.cs
+++ Assets/NikuPrefab.cs
@@ -1,42 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class NikuPrefab : MonoBehaviour
 {
     public float timeOut;
     private float timeElapsed;
+    public int foodRand = 0;
 
+    public int foodTime=0;
 
+    private bool stay;
     // Use this for initialization
     void Start()
     {
-        timeOut = 5.0f;
 
-        // CubeプレハブをGameObject型で取得
-        GameObject obj = (GameObject)Resources.Load("niku2");
-        // Cubeプレハブを元に、インスタンスを生成、
-        Instantiate(obj, new Vector3(0.0f, 2.3f, -7.0f), Quaternion.identity);
     }
 
     //更新
     void Update()
     {
-        timeElapsed += Time.deltaTime;
 
-        if (timeElapsed >= timeOut)
+        //当たっていないとき
+        if(stay==false)
         {
-            // Do anything
-            // CubeプレハブをGameObject型で取得
-            GameObject obj = (GameObject)Resources.Load("niku2");
-            // Cubeプレハブを元に、インスタンスを生成、
-            Instantiate(obj, new Vector3(2.2f, 2.3f, -7.0f), Quaternion.identity);
+            foodTime += 1;
+            if (foodTime > 90)
+            {
+                //乱数
+                System.Random myObject = new System.Random();
+                foodRand = UnityEngine.Random.Range(1, 3); ;
 
-
-            timeElapsed = 0.0f;
+                //乱数で選択された方を生成
+                if (foodRand == 1)
+                {
+                    // Do anything
+                    // CubeプレハブをGameObject型で取得
+                    GameObject obj = (GameObject)Resources.Load("niku2");
+                    // Cubeプレハブを元に、インスタンスを生成、
+                    Instantiate(obj, new Vector3(2.2f,2.5f, -7.0f), Quaternion.identity);
+                }
+                else
+                {
+                    // Do anything
+                    // CubeプレハブをGameObject型で取得
+                    GameObject obj = (GameObject)Resources.Load("Yasai");
+                    // Cubeプレハブを元に、インスタンスを生成、
+                    Instantiate(obj, new Vector3(2.2f, 2.5f, -7.0f), Quaternion.identity);
+                }
+                foodTime = 0;
+            }
         }
- 
-
+        else
+        {
+            //当たっていたらリセットして生成しない
+            foodTime = 0;
+        }
     }
 
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        stay = true;
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        stay = false;
+    }
 }
