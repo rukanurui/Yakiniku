5a561d2a194a15f00a681685a231a397e27ac796
OgasawaraShu <74286718+OgasawaraShu@users.noreply.github.com>
2023-07-12 13:55:28 +0900

    各食材追加

--- Assets/Cigarettes.cs
+++ Assets/Cigarettes.cs
@@ -5,11 +5,22 @@ using UnityEngine;
 
 public class Cigarettes : MonoBehaviour
 {
-    
     //煙草を使用することによってゲージが変化
     public void OnClickCigareButton()
     {
         Gage.instance.cigarettesPoint += 0.5f;
+
         Gage.instance.cigarettesHp += 0.5f;
     }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Gage.instance.cigarettesPoint += 0.5f;
+
+            Gage.instance.cigarettesHp += 0.5f;
+        }
+    }
+   
 }
